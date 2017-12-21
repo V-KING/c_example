a5d4b619ed028b11ea16c8251a78bd0a6c442fba
vk <liu.bao.long.vk@gmail.com>
2017-12-22 01:27:43 +0800

    Add: !!!Template of get xml'note and set xml's node

---
+++ xml_example/SysConf808.h
@@ -0,0 +1,40 @@
+#ifndef _SysConf808_h
+#define _SysConf808_h
+
+/****start <RfidModule>**********************
+<SysConf808>
+    <RfidModule>
+        <IoReset_Flag>yes</IoReset_Flag>
+        <IoReset_Time>fuck</IoReset_Time>
+    </RfidModule>
+</SysConf808>
+*/
+typedef struct RfidModule{
+    /* Must have this menber*/
+    char *nodeName;        
+    /* below according the need to add and define*/
+    char* str_IoReset_Flag; 
+    char* str_IoReset_Time;
+} tSRfidModule;
+/****end  <RfidModule>**********************/
+
+
+/****start <Network>**********************
+<SysConf808>
+    <Network>
+        <ip>yes</ip>
+        <mac>fuck</mac>
+    </Network>
+</SysConf808>
+*/
+typedef struct Network{
+    /* Must have this menber*/
+    char *nodeName;        
+    /* below according the need to add and define*/
+    char* ip; 
+    char* mac;
+} tSNetwork;
+/****end  <Network>**********************/
+
+
+#endif
